7c0e50597520f6c8e88f224a20c4d1e2a43ce746
AsmaHjj <100199289+AsmaHjj@users.noreply.github.com>
2022-02-22 14:34:08 +0000

    Initial commit

---
+++ src/gymple/Gymple.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gymple;
+
+import utils.ConnexionSingleton;
+
+/**
+ *
+ * @author Asma
+ */
+public class Gymple {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       ConnexionSingleton c=ConnexionSingleton.getInstance();
+       System.out.println("o");
+    }
+    
+}
